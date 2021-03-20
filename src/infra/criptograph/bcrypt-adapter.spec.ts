33d5e0fb43818b25c05ee3526afeb7dc199cef52
Yanaiara <yanaiara@alunos.utfpr.edu.br>
2021-03-20 17:51:34 -0300

    test: ensuree BcryptAdapter returns a hash on sucess

--- src/infra/criptograph/bcrypt-adapter.spec.ts
+++ src/infra/criptograph/bcrypt-adapter.spec.ts
@@ -1,13 +1,24 @@
 import bcrypt from 'bcrypt'
+import { resourceUsage } from 'node:process'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 const salt = 12
+jest.mock('bcrypt', () => ({
+    async hash(): Promise<string> {
+        return new Promise((resolve) => resolve('hash'))
+    }
+}))
 
 describe('Bcrypt Adapter', () => {
-    test('SHould call bcrypt with correct values', async () => {
+    test('Should call bcrypt with correct values', async () => {
         const sut = new BcryptAdapter(salt)
         const hashSpy = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt('any_value')
         expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+    test('Should return a hash on sucess', async () => {
+        const sut = new BcryptAdapter(salt)
+        const hash = await sut.encrypt('any_value')
+        expect(hash).toBe('hash')
     })
 })
